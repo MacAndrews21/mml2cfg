2ae6bcf969af34a5c3be56f8d01968cd4b09e09d
MacAndrews21 <walter_andreas_@gmx.de>
2014-12-11 15:02:38 +0100

    some stuff

--- write_tilestache.cfg.py
+++ write_tilestache.cfg.py
@@ -30,8 +30,8 @@ def zoomSteps(name):
 local_url = 'Projekte/mml2cfg/'
 server_url = 'Projekte/mml2cfg/'
 
-url = local_url
-#url = server_url
+#url = local_url
+url = server_url
 #fileNameList = createFileNameList('vector/TileStache/queries/')
 fileNameList = func.createFileNameList(url + 'queries/')
  
